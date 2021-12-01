fb529cf86ee8fb282cdfeb4e5781d3f477bb0b29
wyork <w.york63@gmail.com>
2021-12-01 15:43:38 -0500

    No blank name allowed

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,8 +154,19 @@ var shop = function() {
 }
 };
 
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?")
+  }
+  console.log("Your robot's name is " + name);
+  return name; 
+}
+
+
 // player info
 var playerInfo = {
+  name: getPlayerName (),
   name: window.prompt("What is your robot's name"),
   health: 100,
   attack: 10,
